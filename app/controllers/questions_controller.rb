7642acdc7a9c60751b60a31030a52525a2113104
soutajo <roninghost42@gmail.com>
2023-04-28 20:23:46 +0300

    Added styles for application pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only %i[update show destroy edit]
+  before_action :set_question, only: %i[update show destroy edit]
 
   def create
     question = Question.create(params[:questions])
