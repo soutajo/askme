e170f29c7d8a8a56b6f08401b18117d71f254d89
soutajo <roninghost42@gmail.com>
2023-05-02 23:21:53 +0300

    Added validation of the user data during regisration, password confirmation. Added functionality to edit and delete user profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,47 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    User.create(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'You signed up successfully!'
+    else
+      flash.now[:alert] = 'Incorrect data in registration form. Please try again.'
 
-    redirect_to root_path, notice: 'You signed up successfully!'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'User data updated!'
+    else
+      flash.now[:alert] = 'Error occurred while updating user data.'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'User deleted.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password, :password_confirmation
+    )
   end
 end
